59fe133c3af49d314e78cbce6457e7bb97ad54a9
Kaka <kakas13.souza@gmail.com>
2025-06-04 08:31:03 -0300

    Criando o cadastro de usuarios e produtos

--- ProjetoAsp/Models/Produto.cs
+++ ProjetoAsp/Models/Produto.cs
@@ -2,5 +2,11 @@
 {
     public class Produto
     {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public string Descricao { get; set; }
+        public decimal Preco { get; set; }
+        public decimal quantidade { get; set; }
+
     }
 }
